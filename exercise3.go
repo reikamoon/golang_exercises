118581f78847c9e9a35b543cd30546b9a3f49b48
Anjelica <ambrosio.anjelica@gmail.com>
2021-02-04 16:28:47 -0800

    better house solution

--- exercise3.go
+++ exercise3.go
@@ -11,37 +11,56 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
-  )
-
-
-type House struct {
-   numberOfRooms int
-   city string
-   address string
-   price int
-
+type house struct {
+	address string
+	city    string
+	rooms   int
+	price   int
 }
+
 func main() {
-   var myhouse House
+	var houses []house
+	for i := 1; i != 0; {
+		fmt.Printf("Enter house %d: \n", i)
+		houses = append(houses, enterHouse())
 
-   fmt.Println("Enter how many rooms are in the house. ")
+		fmt.Print("Enter another? (y/n) ")
+		var cont string
+		fmt.Scanf("%s", &cont)
 
-     // var then variable name then variable type
-     var numberOfRooms int
+		if cont != "y" {
+			i = 0
+		} else {
+			i += 1
+		}
+	}
 
-     // Taking input from user
-     fmt.Scanln(&numberOfRooms)
-     fmt.Println("Enter the city: ")
-     var city string
-     fmt.Scanln(&city)
-     fmt.Println("Enter the address: ")
-     var address string
-     fmt.Scanln(&city)
-     fmt.Println("Enter the price: ")
-     var price int
+	for i, h := range houses {
+		fmt.Printf("%d: %-20v\t%-15v\t%-2d Rooms\t$%d\n", i+1, h.address, h.city, h.rooms, h.price)
+	}
+}
 
-     fmt.PrintLn("My house has " + numberOfRooms. "My house is in " + address, "in", + city, "and costs" + price)
+func enterHouse() house {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter address: ")
+	adr, _ := reader.ReadString('\n')
+
+	fmt.Print("Enter city: ")
+	city, _ := reader.ReadString('\n')
+
+	fmt.Print("Enter number of rooms: ")
+	var rooms int
+	fmt.Scanf("%d", &rooms)
+
+	fmt.Print("Enter price: ")
+	var price int
+	fmt.Scanf("%d", &price)
+
+	return house{address: strings.TrimSpace(adr), city: strings.TrimSpace(city), rooms: rooms, price: price}
 }
